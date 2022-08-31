a9ac0f5aa82bd3d68bc0b4e9c47fb4d38462c974
acidburn <cc.acidburn@gmail.com>
2022-08-31 17:02:12 +0200

    Added styling for register page

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -2,16 +2,17 @@ import styles from "./registerpage.module.scss";
 
 export function RegisterPage() {
     return (
-        <div>
+        <div class={styles.register}>
             <form class={styles.register_form}>
-                <label for="username">Username:</label>
-                <input type="text" id="username" />
-                <label for="nickname">Nickname:</label>
-                <input type="text" id="nickname" />
-                <label for="email">Email:</label>
-                <input type="email" id="email" />
-                <label for="pass">Pass:</label>
-                <input type="password" id="pass" />
+                <label class={styles.register_form_label} for="username">Username:</label>
+                <input class={styles.register_form_input} type="text" id="username" />
+                <label class={styles.register_form_label} for="nickname">Nickname:</label>
+                <input class={styles.register_form_input} type="text" id="nickname" />
+                <label class={styles.register_form_label} for="email">Email:</label>
+                <input class={styles.register_form_input} type="email" id="email" />
+                <label class={styles.register_form_label} for="pass">Pass:</label>
+                <input class={styles.register_form_input} type="password" id="pass" />
+                <button class={styles.register_form_btn} type="submit">Register</button>
             </form>
         </div>
     )
