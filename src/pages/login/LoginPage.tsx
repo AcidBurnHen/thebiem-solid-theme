115069b64a990c742ce128431958e2d68237ae9f
acidburn <cc.acidburn@gmail.com>
2022-08-31 18:09:12 +0200

    Added  logic for registration to the registration page, need to make additional changes later.

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -4,8 +4,8 @@ import { LoginUser } from '../../utils/graphQL/mutate/mutate';
 import styles from './loginpage.module.scss';
 
 type LoginProps = {
-  test: string
-}
+  test: string;
+};
 
 export function LoginPage(props: LoginProps) {
   const [formState, setFormState] = createSignal({
@@ -13,7 +13,7 @@ export function LoginPage(props: LoginProps) {
     password: '',
   });
 
-  console.log(props)
+  console.log(props);
 
   const handleName: OnInputEvent = (e) => {
     setFormState({ ...formState(), username: e.currentTarget.value });
@@ -25,23 +25,37 @@ export function LoginPage(props: LoginProps) {
 
   const handleSubmit: OnSubmitForm = async (e) => {
     e.preventDefault();
-    let check = formState().username !== "" && formState().password !== ""
-    let data
-    if(check)
-    data = await LoginUser(formState())
+    let check = formState().username !== '' && formState().password !== '';
+    let data;
+    if (check) data = await LoginUser(formState());
     console.log(data);
     /* Need to decide what approach to use for storing the JWT */
   };
 
-
   return (
     <div class={styles.login}>
-      <form id="form" onSubmit={handleSubmit} class={styles.login_form}>
-        <label class={styles.login_form_label} for='name'>Username:</label>
-        <input class={styles.login_form_input} onInput={handleName} type='text' id='name' />
-        <label class={styles.login_form_label} for='pass'>Password:</label>
-        <input class={styles.login_form_input} onInput={handlePass} type='password' id='pass' />
-        <button class={styles.login_form_btn} type='submit'>Login</button>
+      <form id='form' onSubmit={handleSubmit} class={styles.login_form}>
+        <label class={styles.login_form_label} for='name'>
+          Username:
+        </label>
+        <input
+          class={styles.login_form_input}
+          onInput={handleName}
+          type='text'
+          id='name'
+        />
+        <label class={styles.login_form_label} for='pass'>
+          Password:
+        </label>
+        <input
+          class={styles.login_form_input}
+          onInput={handlePass}
+          type='password'
+          id='pass'
+        />
+        <button class={styles.login_form_btn} type='submit'>
+          Login
+        </button>
       </form>
     </div>
   );
--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,19 +1,67 @@
-import styles from "./registerpage.module.scss";
+import { createSignal } from 'solid-js';
+import { OnInputEvent, OnSubmitForm } from '../../types/event-types';
+import { RegisterUser } from '../../utils/graphQL/mutate/mutate';
+import styles from './registerpage.module.scss';
 
 export function RegisterPage() {
-    return (
-        <div class={styles.register}>
-            <form class={styles.register_form}>
-                <label class={styles.register_form_label} for="username">Username:</label>
-                <input class={styles.register_form_input} type="text" id="username" />
-                <label class={styles.register_form_label} for="nickname">Nickname:</label>
-                <input class={styles.register_form_input} type="text" id="nickname" />
-                <label class={styles.register_form_label} for="email">Email:</label>
-                <input class={styles.register_form_input} type="email" id="email" />
-                <label class={styles.register_form_label} for="pass">Pass:</label>
-                <input class={styles.register_form_input} type="password" id="pass" />
-                <button class={styles.register_form_btn} type="submit">Register</button>
-            </form>
-        </div>
-    )
+  const [formState, setFormState] = createSignal({
+    username: '',
+    nickname: '',
+    email: '',
+    password: '',
+  });
+
+  const handleUsername: OnInputEvent = (e) => {
+    setFormState({ ...formState(), username: e.currentTarget.value });
+  };
+
+  const handleNickname: OnInputEvent = (e) => {
+    setFormState({ ...formState(), nickname: e.currentTarget.value });
+  };
+
+  const handleEmail: OnInputEvent = (e) => {
+    setFormState({ ...formState(), email: e.currentTarget.value });
+  };
+
+  const handlePass: OnInputEvent = (e) => {
+    setFormState({ ...formState(), password: e.currentTarget.value });
+  };
+
+  const handleSubmit: OnSubmitForm = async (e) => {
+    e.preventDefault();
+    let check = formState().username !== '' && formState().nickname !== '' && formState().email !== '' && formState().password !== '';
+    let data;
+    if (check) 
+    /* Need to add a conditional check to see if the user already exists in the database before trying to 
+    register, there are issues with wpGraphQL query for fetching by username so this will be added later */
+    data = await RegisterUser(formState());
+    console.log(data);
+   
+  };
+
+  return (
+    <div class={styles.register}>
+      <form class={styles.register_form}>
+        <label class={styles.register_form_label} for='username'>
+          Username:
+        </label>
+        <input onInput={handleUsername} class={styles.register_form_input} type='text' id='username' />
+        <label class={styles.register_form_label} for='nickname'>
+          Nickname:
+        </label>
+        <input onInput={handleNickname} class={styles.register_form_input} type='text' id='nickname' />
+        <label class={styles.register_form_label} for='email'>
+          Email:
+        </label>
+        <input onInput={handleEmail} class={styles.register_form_input} type='email' id='email' />
+        <label class={styles.register_form_label} for='pass'>
+          Password:
+        </label>
+        <input onInput={handlePass} class={styles.register_form_input} type='password' id='pass' />
+        <button class={styles.register_form_btn} type='submit'>
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
