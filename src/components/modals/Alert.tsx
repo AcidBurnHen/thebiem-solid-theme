65a2192157d2c5a01c30ccd83ea2756da295af7d
acidburn <cc.acidburn@gmail.com>
2022-09-02 20:27:33 +0200

    Created a re-usable alert component

---
+++ src/components/modals/Alert.tsx
@@ -0,0 +1,21 @@
+import { Portal, Show } from 'solid-js/web';
+import styles from './alert.module.scss';
+
+interface AlertProps {
+  show: boolean;
+  alertType: "good" | "bad"
+  el: string;
+  msg: string;
+}
+
+export function Alert(props: AlertProps) {
+  return (
+    <Show when={props.show}>
+      <Portal mount={document.getElementById(props.el) as HTMLElement}>
+        <div class={`${styles.alert} ${props.alertType === "good" ? styles.alert_good : styles.alert_bad}`}>
+          <p class={styles.alert_msg}>{props.msg}</p>
+        </div>
+      </Portal>
+    </Show>
+  );
+}
